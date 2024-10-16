1fb3c46389fcc2c872b786dd7b1383502c928201
Aryansingh3576 <singharyan35764166@gmail.com>
2024-10-16 13:37:11 +0000

    proogram 1

---
+++ test/1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+
+int a,b,sum,sub,prod,div;
+printf("Enter Two Numbers\n");
+scanf("%d%d",&a,&b);
+sum = a + b ;
+sub = a - b ;
+prod = a * b;
+div = a / b ;
+printf("sum=%d\n",sum);
+printf("sub=%d\n",sub);
+printf("prod=%d\n",prod);
+printf("div=%d\n",div);
+
+return 0;
+
+}
--- test/1.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using std::cout;
-using std::endl;
-int main(){
-cout << "hello chai from ram" << endl;
-return 0;
-}
